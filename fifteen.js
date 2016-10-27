8a91783917f86f07e19578fab360771659a99a9b
Alton Bodley <altonbodley@gmail.com>
2016-10-27 15:00:00 -0500

    Move pieces
    Pieces surriunding a blank space can be moved into that blank space.

--- fifteen.js
+++ fifteen.js
@@ -1,11 +1,14 @@
-
+/*Initial position of blank piece*/
+var tBlank= 300;//top of blank piece
+var lBlank= 300;//left margin of blank piece
+var piece=[];
 window.onload = function(){
 	puzzle();
+	
 	//h.r$('shufflebutton').observe('click', shuffle);
 }
 
 function puzzle(){
-	var piece=[];
 	piece= $('#puzzlearea div');
 	$('#puzzlearea div').addClass('puzzlepiece');
 	var a = 0;
@@ -15,6 +18,42 @@ function puzzle(){
 			piece[i].style.top = 100 * a + "px";
 			piece[i].style.left = 100 * x  + "px";
 			piece[i].style.backgroundPosition = -x * 100 + "px " + a * -100 + "px";
+			piece[i].addEventListener('mouseenter',function(){
+				if (Math.abs(tBlank - parseInt(this.style.top)) == 100) {
+					if (Math.abs(lBlank - parseInt(this.style.left)) == 0) {
+						movablepiece(this); 
+					}
+				} 
+				else if (Math.abs(lBlank - parseInt(this.style.left)) == 100) {
+					if (Math.abs(tBlank - parseInt(this.style.top)) == 0) {
+						movablepiece(this);
+					}
+				}
+				})			
+			piece[i].addEventListener('click',function(){
+				if($(this).hasClass('movablepiece')){
+					var ct=parseInt(this.style.top);
+					var cl=parseInt(this.style.left);
+					var tempT=tBlank;
+					var tempL=lBlank;
+					tBlank=ct;
+					lBlank=cl;
+					this.style.top = tempT +'px';
+					this.style.left = tempL +'px';
+				}
+			},true)
+			piece[i].addEventListener('mouseleave',function(){
+				if (Math.abs(tBlank - parseInt(this.style.top)) == 100) {
+					if (Math.abs(lBlank - parseInt(this.style.left)) == 0) {
+						nmovablepiece(this); 
+					}
+				} 
+				else if (Math.abs(lBlank - parseInt(this.style.left)) == 100) {
+					if (Math.abs(tBlank - parseInt(this.style.top)) == 0) {
+						nmovablepiece(this);
+					}
+				}
+				})
 			i++;
 		}
 		a++;
@@ -24,4 +63,12 @@ function puzzle(){
 		i--;
 		
 	};
+}
+
+function movablepiece(x){
+	//console.log(x)
+	$(x).addClass("movablepiece"); 
+}
+function nmovablepiece(x){
+	$(x).removeClass("movablepiece");
 }
